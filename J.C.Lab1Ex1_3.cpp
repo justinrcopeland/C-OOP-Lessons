b286d13c78c372dc0d7f839fd3bd3af92fd7bb87
justinrcopeland <120751800+justinrcopeland@users.noreply.github.com>
2024-04-20 10:02:42 -0500

    Add files via upload

---
+++ J.C.Lab1Ex1_3.cpp
@@ -0,0 +1,27 @@
+// Justin Copeland
+// Cosc1435/004
+// Ex 1.3
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	float number;
+	int divider;
+	
+	divider = 2;
+
+	cout << "Hi there" << endl;
+	cout << "Please input a number and then hit return" << endl;
+	cin  >> number;
+	
+	number = number / divider;
+	
+	cout << "Half of your number is " << number << endl;
+	
+	return 0;
+}

5d2e351e681f524dd48f9c690d56ec79067a00de
justinrcopeland <120751800+justinrcopeland@users.noreply.github.com>
2024-04-20 10:01:52 -0500

    Create lab1.cpp

---
+++ lab1.cpp
@@ -0,0 +1,13 @@
+// Justin Copeland
+// Cosc 1436/004
+// Ex 1.1
+
+#include <iostream> // needed to perform C++ I/O
+using namespace std;
+
+int main ()
+{
+	cout << "Now is the time for all good men" << endl;
+	cout << "To come to the aid of their party" << endl;
+	return 0;
+}
